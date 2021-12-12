3b91f86f3901c9032301e9c88b5a1f693db603ff
terencech <terence000@gmail.com>
2021-12-11 20:55:58 -0800

    add public path to app.use

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ var app = express();
 console.log("Hello World");
 
 app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
     absolutePath = __dirname + "/views/index.html";
