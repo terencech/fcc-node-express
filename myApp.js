f735be9377b4b6ce5a520a3506d22197b8cebb0d
terencech <terence000@gmail.com>
2021-12-11 20:38:42 -0800

    change get / to respond with views/index.html

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ var app = express();
 console.log("Hello World")
 
 app.get("/", function (req, res) {
-    res.send("Hello Express");
+    absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
 });
 
 
